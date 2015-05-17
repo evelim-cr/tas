0a3b2fb7964ad9d2ad3141d99effa18b24fd0011
AlanPCS-Elementary-vm <alanpcsilva@gmail.com>
2015-05-17 17:56:01 -0300

    Add seeds with post sources to the database
    Signed-off-by: AlanPCS-Elementary-vm <alanpcsilva@gmail.com>

--- Tas/db/seeds.rb
+++ Tas/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Source.create(name: 'Reddit')
+Source.create(name: 'Youtube')
+Source.create(name: 'Twitter')
