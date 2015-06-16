a790336d9d7c3dcb2a6d78258a56951f2749298d
AlanPCS <apcs11@inf.ufpr.br>
2015-06-16 18:53:02 -0300

    Melhorada a página de resultados, ainda com posts

--- Tas/app/controllers/application_controller.rb
+++ Tas/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # Pagina 404
-  config.action_dispatch.rescue_responses.merge!(
+  config.action_dispatch(
     'MyCustomException' => :not_found
   )
 end
